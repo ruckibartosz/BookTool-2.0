6aad9212e0a01a7990ec1f837ad53014e02e108d
brucki <ruckibartosz@gmail.com>
2023-01-08 15:50:09 +0100

    added to connection page grid

--- booktool-frontend/src/Pages/Connection/Connection.tsx
+++ booktool-frontend/src/Pages/Connection/Connection.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { FcGoogle } from 'react-icons/fc';
+import { TbBrandBooking } from 'react-icons/tb';
 
 import PageContainer from '@Components/PageContainer';
 import SyncCard from '@Components/SyncCard';
-import ListTilesView from '@Components/ListTilesView';
-import ListTilesViewItem from '@Components/ListTilesView/ListTilesViewItem';
 
 const Connection: React.FC = () => {
     return (
         <PageContainer>
-            <ListTilesView>
-                <ListTilesViewItem>
-                    <SyncCard icon={<FcGoogle size={58} />} title="Google" isSynchronized={true}/>
-                </ListTilesViewItem>
-            </ListTilesView>
+            <Grid templateColumns={['1fr', '1fr', '1fr', 'repeat(2, 1fr)', 'repeat(3, 1fr)']} gap={6}>
+                <GridItem>
+                    <SyncCard icon={<FcGoogle size={58} />} title="Google" isSynchronized={true} />
+                </GridItem>
+                <GridItem>
+                    <SyncCard
+                        icon={<TbBrandBooking size={58} />}
+                        title="Booking.com"
+                        isSynchronized={false}
+                    />
+                </GridItem>
+            </Grid>
         </PageContainer>
     );
 };
