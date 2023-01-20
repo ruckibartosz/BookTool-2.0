9b0ec437ccd563841faa0b4e07d45cf2a02c19c5
brucki <ruckibartosz@gmail.com>
2023-01-20 17:56:24 +0100

    delete backend folder

--- booktool-backend/src/databases/index.ts
+++
@@ -1,5 +0,0 @@
-import { DB_HOST, DB_PASSWORD, DB_DATABASE } from '@config';
-
-export const dbConnection = {
-  url: `mongodb+srv://${DB_HOST}:${DB_PASSWORD}@${DB_DATABASE}.mongodb.net/?retryWrites=true&w=majority`,
-};
